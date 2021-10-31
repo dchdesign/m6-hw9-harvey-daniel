73cb17ca735e1f40c1f130dcb8ce297d7985f39e
dan harvey <daniel.harvey@ufl.edu>
2021-10-31 16:08:37 -0400

    console.log change

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ console.log(formEl,inputEL,weatherEL)
 formEl.onsubmit = function(e) {
     e.preventDefault()
     var query = inputEl.value
-    console.log(query)
+    // console.log(query)
     fetch('https://api.openweathermap.org/data/2.5/weather?q=&units=imperial&appid=528fbf428cefbb3ae73ba5ed4014a523=' + query)
     .then(function(response) {
         return response.json()
-    }
-    .then(function(result) {
-        renderWeather(result)
     })
-
+    .then(function(result) {
+        console.log(result)
+        console.log('weather')
+    })
+}
 
 
     // function renderWeather(weatherObject) {
