d73ce04aaad10ec35e8f5b22f4f32d0617880f2b
dan harvey <daniel.harvey@ufl.edu>
2021-10-31 17:58:04 -0400

    fixed fetch

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ formEl.onsubmit = function(e) {
     e.preventDefault()
     var query = inputEl.value
     // console.log(query)
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=&units=imperial&appid=528fbf428cefbb3ae73ba5ed4014a523=' + query)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=&units=imperial&appid=528fbf428cefbb3ae73ba5ed4014a523' + query)
     .then(function(response) {
         return response.json()
     })
