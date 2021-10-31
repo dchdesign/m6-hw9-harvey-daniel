a7b81e0072e31573cd63ad0d60200b54ff1051a5
dan harvey <daniel.harvey@ufl.edu>
2021-10-30 21:20:09 -0400

    styles and js entries

---
+++ index.js
@@ -0,0 +1,29 @@
+// var btn = document.querySelector('button')
+// var forecastEl = document.getElementById('forecast')
+
+var formEl = document.querySelector('form')
+var inputEL = document.querySelector('input[type="text"]')
+var weatherEL = document.getElementById('weather')
+console.log(formEl,inputEL,weatherEL)
+
+
+
+
+// btn.onclick = function() {
+//     //console.log('clicked')
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q=/^\w+\s\w+$/g&units=imperial&appid=528fbf428cefbb3ae73ba5ed4014a523')
+//     .then(function(res) {
+//         return res.json()
+//     })
+//     .then(function(res) {
+//         console.log(res)
+//     })
+
+//     res.results.forEach(function() {
+//         var container = document.createElement('div')
+//         var img = document.createElement('img')
+//         img.src = 
+//         container.appendChild
+//     })
+// }
+
